d0daaa927f56e4f2727a8ff24b37188b85de9252
Pablorde9 <161084911+Pablorde9@users.noreply.github.com>
2025-09-09 16:00:49 -0300

    vai toma no cu motorista do uber

---
+++ Jogador.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private ArrayList<Cartas> mao;
+
+    public Jogador(String n) {
+        this.nome = n;
+        this.mao = new ArrayList<>();
+    }
+
+    public void receberCarta(Cartas carta) {
+        if (this.mao.size() < 3) {
+            this.mao.add(carta);
+        }
+    }
+
+    public Cartas jogarCarta(Cartas carta) {
+        if (this.mao.remove(carta)) {
+            return carta;
+        }
+        return null;
+    }
+
+    public void limparMao() {
+        this.mao.clear();
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public List<Cartas> getMao() {
+        return new ArrayList<>(this.mao);
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
